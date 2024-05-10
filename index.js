42ac5b18155b1ed97eae22dcf2fa3e0c61eb214d
dtewyhujsi <gs1peach792@hotmail.com>
2024-05-11 04:29:14 +0700

    update

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-const initializeArrayWithRange = (end, start = 0) =>
-  Array.from({ length: end - start }, (_, i) => i + start);
+const fibonacci = (n) => {
+  if (n <= 1) {
+    return n;
+  }
+  let prev = 0;
+  let curr = 1;
+  for (let i = 2; i <= n; i++) {
+    const next = prev + curr;
+    prev = curr;
+    curr = next;
+  }
+  return curr;
+};
